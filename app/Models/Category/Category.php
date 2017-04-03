af1cbf84558cf0307561206cace8aaf922a4569b
ketu.lai <ketu.lai@gmail.com>
2017-04-03 17:57:31 +0800

    add category model

---
+++ app/Models/Category/Category.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: ketu.lai <ketu.lai@gmail.com>
+ * Date: 2017/4/3 17:50
+ */
+
+namespace App\Models\Category;
+
+use Dimsav\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Model;
+class Category
+{
+
+}
---
+++ app/Models/Category/CategoryTranslation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: ketu <ketu.lai@gmail.com>
+ * Date: 17-3-26
+ */
+
+namespace App\Models\Category;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryTranslation extends Model
+{
+    //table name
+    protected $table = 'attribute_translations';
+
+    public $timestamps = false;
+
+    protected $fillable = ['name', 'value', 'locale'];
+
+}
